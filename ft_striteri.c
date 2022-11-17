bf9acbde43737df957d19feb012ce0172bffcf59
Sara <samarque@student.42lisboa.com>
2022-11-17 02:14:03 +0000

    pt2

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,13 +6,27 @@
 /*   By: sara <sara@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 19:38:57 by sara              #+#    #+#             */
-/*   Updated: 2022/11/14 17:12:27 by sara             ###   ########.fr       */
+/*   Updated: 2022/11/17 01:58:04 by sara             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*Applies the function f to each character of the string passed as argument, 
+and passing its index as first argument. Each character is passed by
+address to f to be modified if necessary.
+PARAMETERS
+#1. The string on which to iterate.
+#2. The function to apply to each character.*/
+
 #include "libft.h"
 
-void		ft_striteri(char *s, void (*f)(unsigned int, char*))
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
-	
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+	{
+		f(i, &s[i]);
+		i++;
+	}
 }
--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: sara <sara@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 19:38:30 by sara              #+#    #+#             */
-/*   Updated: 2022/11/13 18:54:27 by sara             ###   ########.fr       */
+/*   Updated: 2022/11/17 02:06:21 by sara             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,19 @@ Returns NULL if the allocation fails.*/
 
 #include "libft.h"
 
-char		*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	
+	char	*str;
+	size_t	i;
+
+	str = ft_strdup(s);
+	if (!str)
+		return (0);
+	i = 0;
+	while (str[i])
+	{
+		str[i] = f(i, str[i]);
+		i++;
+	}
+	return (str);
 }
